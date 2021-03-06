26c8463cf2abdde02e8c6ac602c0ee9372179755
kanta13jp1 <kanta13jp@gmail.com>
2021-03-07 02:00:38 +0900

    use OpenCV

---
+++ temp.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import cv2
+
+img = cv2.imread('example2.jpg')
+Z = img.reshape((-1,3))
+Z = np.float32(Z)
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+K = 256
+ret,label,center=cv2.kmeans (Z,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+center = np.uint8(center)
+res = center[label.flatten()]
+res2 = res.reshape((img.shape))
+
+cv2.imshow('res2',res2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
