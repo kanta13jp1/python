9d4a2f020d2af6afa149091d326d2ea1980f69f5
kanta13jp1 <kanta13jp@gmail.com>
2022-03-25 08:50:59 +0900

    String, list

--- day1.py
+++ day1.py
@@ -1,14 +1,29 @@
 #Int data Type
+
 var = 5
 print(var)
 print(type(var))
 
 #Float data type
+
 var2 = 0.1234
 print(var2)
 print(type(var2))
 
 #Complex numbers data type
+
 x = 2j
 print(x)
 print(type(x))
+
+#String Data Type
+
+str_one = "Hello World"
+print(str_one)
+print(type(str_one))
+
+#list Data type
+
+list1 = ["car","bike"]
+print(list1)
+print(type(list1))
