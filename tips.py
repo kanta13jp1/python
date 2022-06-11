15462df69b024e0c8a326735adb90d92ac278775
kanta13jp1 <kanta13jp@gmail.com>
2022-06-11 13:39:10 +0900

    ImageDraw

--- tips.py
+++ tips.py
@@ -1,3 +1,15 @@
+from PIL import Image, ImageDraw 
+
+image = Image.new("RGB", (400, 400), "blue" ) 
+
+draw = ImageDraw.Draw( image) 
+
+draw.rectangle((200, 100, 300, 200), fill="red") 
+
+draw.rectangle((50, 50, 150, 150), fill="green", outline="yellow", width=3) 
+
+image.save("output.jpg")
+
 my_list = list('abcc')
 
 print(my_list)
