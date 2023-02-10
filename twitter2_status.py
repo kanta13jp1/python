46d4acab2362248e5cf9414af9ee517a008a75a6
kanta13jp1 <kanta13jp@gmail.com>
2023-02-11 02:52:41 +0900

    mongoDB接続

--- twitter2_status.py
+++ twitter2_status.py
@@ -14,43 +14,53 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
 user_list = {
-    'CDP2017',
+    'hashimoto_lo',
+    'kokumin_okayama',
+    'kokumin_toyama',
+    'kokumin_kyoto',
+    'kokuminibaraki',
+    'kokumin_myzk',
+    'kokumin_tks',
+    'newkokuminnara',
+    'kokumin_fukuoka',
+    'dpfpmiyazaki1',
+    'kokumin_ymgc',
+    'kokumin_fukui',
+    'kokumin_chiba',
+    'kokumin_kng',
+    'kokumin_aichi_',
+    'f7SYmFBYMivVBCE',
+    'KokuminHokkaido',
+    'kagawakokumin',
+    'kokumin_osaka',
+    'dpfp_shiga',
+    'dpfptokyo',
+    'KokuminOita',
+    'kokumin_ngsk',
+    'gokokuminymgt',
+    'kokumin_mie',
+    'dpfp_shimane',
+    'dp_tochigi',
+    'n7e0jof',
+    'minshin_y',
+    'dpj_kagoshima',
+    'minshuwakayama',
     'DPFPnews',
 }
 
 members_list = []
 
-rikken_follower = 0
-rikken_sabun = 0
-kokumin_follower = 0
-kokumin_sabun = 0
-sabun = 0
-
 for screen_name in user_list:
     print(screen_name)
     user = api.get_user(screen_name=screen_name)
-
-    if user.name == '立憲民主党':
-        rikken_follower: int = user.followers_count
-        user_info = [user.name, '@' + user.screen_name,
-                     rikken_follower, rikken_follower - 191616]
-    else:
-        kokumin_follower: int = user.followers_count
-        user_info = [user.name, '@' + user.screen_name,
-                     kokumin_follower, kokumin_follower - 56302]
-    print(user_info)
+    user_info = [user.name, '@' + user.screen_name,
+                 user.statuses_count, user.followers_count]
+    # print(user_info)
+    # if (user.description == '' and user.statuses_count != 0):
     members_list.append(user_info)
-
-print(f'{rikken_follower} and {kokumin_follower}')
-
-sabun = (rikken_follower - kokumin_follower)
-print(sabun)
-user_info = ['差分', '', sabun, sabun - 135314]
 
 
 sorted_list = sorted(members_list, key=itemgetter(2), reverse=True)
-sorted_list.append(user_info)
-
 print(*sorted_list, sep='\n')
 # def follower_counts_func(acount):
 #    followers_list = []
@@ -125,7 +135,7 @@ try:
 except FileExistsError:
     pass
 df = pd.DataFrame(sorted_list, columns=[
-                  'name', 'screen_name', 'followers_count', 'sabun'])
+                  'name', 'screen_name', 'statuses_count', 'followers_count'])
 df.to_csv(dir + '/sorted_users_' +
           datetime.datetime.now().strftime('%y%m%d%H%M%S') + '.csv')
 # removed_users.to_csv(dir + '/removed_users_' + datetime.datetime.now().strftime('%y%m%d%H%M%S') + '.csv')
